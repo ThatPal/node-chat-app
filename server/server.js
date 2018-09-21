0b10155b335cfcab53ff9c469bf81655151ae4dd
Mordechai Pal <mordechaipal@gmail.com>
2018-09-20 23:48:18 -0400

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', socket => {
     console.log('New user connected');
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined chat',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', message => {
         console.log('createMessage', message);
@@ -20,7 +31,7 @@ io.on('connection', socket => {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
     });
 
     socket.on('disconnect', message => {
