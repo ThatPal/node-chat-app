374bf373b90be0214b1e6ea28b7ae3d56e690339
Mordechai Pal <mordechaipal@gmail.com>
2018-09-21 13:10:14 -0400

    Add css for chat

--- public/js/index.js
+++ public/js/index.js
@@ -28,8 +28,14 @@ socket.on('newLocationMessage', function (message) {
 
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
-    socket.emit('createMessage', {from: 'User', text: $('[name=message]').val()}, function () {
 
+    let messageTextbox = $('[name=message]');
+
+    socket.emit('createMessage', {
+        from: 'User', 
+        text: messageTextbox.val()
+    }, function () {
+        messageTextbox.val('');
     });
 });
 
@@ -38,12 +44,15 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.')
     });
 });
